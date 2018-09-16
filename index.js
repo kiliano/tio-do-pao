1afc25ef61a23b875671f9d4a66bd4add0507c00
Kiliano Lopes <kilianolopes@gmail.com>
2018-09-16 14:30:06 -0300

    dasdas

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var port = (process.env.PORT || 5000)
 
 http.createServer(function(request, response) {
 	response.writeHead(200,{'Content-Type': 'application/json'});
-	response.write(JSON.stringify({name: 'tiodopaobot', ver'0.1'}));
+	response.write(JSON.stringify({name: 'tiodopaobot', ver: '0.1'}));
 	response.end();
 }).listen(port)
 
