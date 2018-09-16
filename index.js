e3cc521fff0763d102491ee42e42f4ac8038b683
Kiliano Lopes <kilianolopes@gmail.com>
2018-09-16 14:28:55 -0300

    asdas

--- index.js
+++ index.js
@@ -3,6 +3,15 @@
 
 // Chamads do Heroku
 
+var http = require('http')
+var port = (process.env.PORT || 5000)
+
+http.createServer(function(request, response) {
+	response.writeHead(200,{'Content-Type': 'application/json'});
+	response.write(JSON.stringify({name: 'tiodopaobot', ver'0.1'}));
+	response.end();
+}).listen(port)
+
 var token = process.env.token
 
 // Chamando bases
