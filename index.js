3debed75aea0e857b6ee52c9dee41132cbe30c59
Kiliano Lopes <kiliano.lopes@degraupublicidade.com.br>
2018-10-30 11:00:51 -0300

    Consertando bug de relatório mensal

--- index.js
+++ index.js
@@ -67,66 +67,66 @@ const ctx = {}
 
 
 // Chamadas para o Local
-	// const env = require('./.env');
-	// const bot = new Telegraf(env.token);
-	// const telegram = new Telegram(env.token);
+	const env = require('./.env');
+	const bot = new Telegraf(env.token);
+	const telegram = new Telegram(env.token);
 
-	// const apiUrl = env.apiUrl;
-	// const apiFileUrl = env.apiFileUrl;
+	const apiUrl = env.apiUrl;
+	const apiFileUrl = env.apiFileUrl;
 
-	// const idKiliano = env.idKiliano;
-	// const idBartira = env.idBartira;
-	// const idGeovana = env.idGeovana;
-	// const idRodrigo = env.idRodrigo;
-	// const idIsabel = env.idIsabel;
-	// const idChatDegrau = env.idChatDegrau;
-	// const idChatFronts = env.idChatFronts;
-	// const apiTinypng = env.apiTinypng;
+	const idKiliano = env.idKiliano;
+	const idBartira = env.idBartira;
+	const idGeovana = env.idGeovana;
+	const idRodrigo = env.idRodrigo;
+	const idIsabel = env.idIsabel;
+	const idChatDegrau = env.idChatDegrau;
+	const idChatFronts = env.idChatFronts;
+	const apiTinypng = env.apiTinypng;
 	
 
-	// const idTodos = env.idTodos;
+	const idTodos = env.idTodos;
 
 
-	// const apiClimatempo = env.apiClimatempo;
+	const apiClimatempo = env.apiClimatempo;
 
-	// const wordpressPass = env.wordpressPass;
+	const wordpressPass = env.wordpressPass;
 
-	// fuso = 0;
+	fuso = 0;
 
 
 // Chamadas para o Heroku
 
-	var port = (process.env.PORT || 5000)
+	// var port = (process.env.PORT || 5000)
 
-	http.createServer(function(request, response) {
-		response.writeHead(200,{'Content-Type': 'application/json'});
-		response.write(JSON.stringify({name: 'Acorda Horacio', ver: '1.0'}));
-		response.end();
-	}).listen(port)
+	// http.createServer(function(request, response) {
+	// 	response.writeHead(200,{'Content-Type': 'application/json'});
+	// 	response.write(JSON.stringify({name: 'Acorda Horacio', ver: '1.0'}));
+	// 	response.end();
+	// }).listen(port)
 
-	const token = process.env.token
+	// const token = process.env.token
 
-	const idKiliano = process.env.idKiliano
-	const idBartira = process.env.idBartira
-	const idRodrigo = process.env.idRodrigo;
-	const idGeovana = process.env.idGeovana;
-	const idIsabel = process.env.idIsabel;
-	const idChatDegrau = process.env.idChatDegrau
-	const idChatFronts = process.env.idChatFronts
-	const wordpressPass = process.env.wordpressPass;
+	// const idKiliano = process.env.idKiliano
+	// const idBartira = process.env.idBartira
+	// const idRodrigo = process.env.idRodrigo;
+	// const idGeovana = process.env.idGeovana;
+	// const idIsabel = process.env.idIsabel;
+	// const idChatDegrau = process.env.idChatDegrau
+	// const idChatFronts = process.env.idChatFronts
+	// const wordpressPass = process.env.wordpressPass;
 
-	const apiTinypng = process.env.apiTinypng;
+	// const apiTinypng = process.env.apiTinypng;
 
-	const idTodos = process.env.idTodos
+	// const idTodos = process.env.idTodos
 
-	const apiUrl = `https://api.telegram.org/bot${token}`
-	const apiFileUrl = `https://api.telegram.org/file/bot${token}`
+	// const apiUrl = `https://api.telegram.org/bot${token}`
+	// const apiFileUrl = `https://api.telegram.org/file/bot${token}`
 
-	const apiClimatempo = process.env.apiClimatempo
+	// const apiClimatempo = process.env.apiClimatempo
 
-	const bot = new Telegraf(token)
-	const telegram = new Telegram(token);
-	fuso = 3;
+	// const bot = new Telegraf(token)
+	// const telegram = new Telegram(token);
+	// fuso = 3;
 
 
 
@@ -1516,6 +1516,7 @@ const relatoriopaodetalhadoprint = (ctx, next) => {
 
 const relatoriopaobartira = (ctx, next) => {
 	msg(''+pedidosanalisadossoma.lista+'',idKiliano)
+	msg(''+pedidosanalisadossoma.lista+'',idBartira)
 	next();
 }
 
@@ -1655,14 +1656,23 @@ const eventosagendados = (ctx, next) => {
 
 
 	// ------ Mandando relatório mensal para Bartira
-	var scheduleteste = schedule.scheduleJob({day: 1, hour: 8+fuso, minute: 30}, function(){
-		if (pedido.mes_data == 1) {
+	// var scheduleteste = schedule.scheduleJob({hour: 10+fuso, minute: 31, dayOfWeek: [1,2,3,4,5,6,7], dayOfMonth: 1}, function(){
+	// 	if (pedido.mes_data == 1) {
+	// 		relatorioTempo = [1,12,(pedido.ano_data-1)];
+	// 	} else {
+	// 		relatorioTempo = [1,(pedido.mes_data-1),pedido.ano_data];
+	// 	}
+	// 	exec(ctx, atualizarData, carregartodos, relatoriopao, relatoriopaodetalhado, relatoriopaobartira, liberandopost)
+
+	// });
+
+	var schedulerelatoriomensal = schedule.scheduleJob({hour: 8+fuso, minute: 15, dayOfWeek: 1, dayOfMonth: [1,2,3,4,5,6,7]}, function(){
+	  if (pedido.mes_data == 1) {
 			relatorioTempo = [1,12,(pedido.ano_data-1)];
 		} else {
 			relatorioTempo = [1,(pedido.mes_data-1),pedido.ano_data];
 		}
 		exec(ctx, atualizarData, carregartodos, relatoriopao, relatoriopaodetalhado, relatoriopaobartira, liberandopost)
-
 	});
 	
 
