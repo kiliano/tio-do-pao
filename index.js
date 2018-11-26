189686e8eb8cfb65aad0b940c52cf62b5bc3bde7
Kiliano Lopes <kiliano.lopes@degraupublicidade.com.br>
2018-11-26 17:27:35 -0200

    Gerador de CPF e CNPJ válidos. Correção de fuso

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ const ctx = {}
 
 	const bot = new Telegraf(token)
 	const telegram = new Telegram(token);
-	fuso = 3;
+	fuso = 2;
 
 
 
@@ -205,6 +205,80 @@ const msg = (msg, id) => {
 	axios.get(`${apiUrl}/sendMessage?chat_id=${id}&text=${encodeURI(msg)}`)
 		.catch(e => console.log(e))
 }
+
+
+// CPF
+
+// CPF E CNPJ
+
+// ****************************************
+// Script Gerador de CPF e CNPJ VÃ¡lidos
+// Autor: Marcos Guiga
+// Site : Worldigital.co.cc
+// Email: marcosguiga@hotmail.com
+// Data:  19/12/2010
+// ****************************************
+// FunÃ§Ã£o para gerar nÃºmeros randÃ´micos
+
+const gera_random = (n) =>{
+    var ranNum = Math.round(Math.random()*n);
+    return ranNum;
+}
+
+// FunÃ§Ã£o para retornar o resto da divisao entre nÃºmeros (mod)
+const mod = (dividendo,divisor) => {
+          return Math.round(dividendo - (Math.floor(dividendo/divisor)*divisor));
+}
+
+// FunÃ§Ã£o que gera nÃºmeros de CPF vÃ¡lidos
+const cpf = () => {
+          var n = 9;
+          var n1 = gera_random(n);
+           var n2 = gera_random(n);
+           var n3 = gera_random(n);
+           var n4 = gera_random(n);
+           var n5 = gera_random(n);
+           var n6 = gera_random(n);
+           var n7 = gera_random(n);
+           var n8 = gera_random(n);
+           var n9 = gera_random(n);
+           var d1 = n9*2+n8*3+n7*4+n6*5+n5*6+n4*7+n3*8+n2*9+n1*10;
+           d1 = 11 - ( mod(d1,11) );
+           if (d1>=10) d1 = 0;
+           var d2 = d1*2+n9*3+n8*4+n7*5+n6*6+n5*7+n4*8+n3*9+n2*10+n1*11;
+           d2 = 11 - ( mod(d2,11) );
+           if (d2>=10) d2 = 0;
+           return ''+n1+n2+n3+'.'+n4+n5+n6+'.'+n7+n8+n9+'-'+d1+d2;
+}
+
+const cnpj = () => {
+          var n = 9;
+          var n1  = gera_random(n);
+           var n2  = gera_random(n);
+           var n3  = gera_random(n);
+           var n4  = gera_random(n);
+           var n5  = gera_random(n);
+           var n6  = gera_random(n);
+           var n7  = gera_random(n);
+           var n8  = gera_random(n);
+           var n9  = 0;//gera_random(n);
+           var n10 = 0;//gera_random(n);
+           var n11 = 0;//gera_random(n);
+           var n12 = 1;//gera_random(n);
+          var d1 = n12*2+n11*3+n10*4+n9*5+n8*6+n7*7+n6*8+n5*9+n4*2+n3*3+n2*4+n1*5;
+           d1 = 11 - ( mod(d1,11) );
+           if (d1>=10) d1 = 0;
+           var d2 = d1*2+n12*3+n11*4+n10*5+n9*6+n8*7+n7*8+n6*9+n5*2+n4*3+n3*4+n2*5+n1*6;
+           d2 = 11 - ( mod(d2,11) );
+           if (d2>=10) d2 = 0;
+           return ''+n1+n2+'.'+n3+n4+n5+'.'+n6+n7+n8+'/'+n9+n10+n11+n12+'-'+d1+d2;
+}
+
+
+
+
+
+
 
 
 const carregarum = (ctx, next) => {
@@ -1645,21 +1719,8 @@ const eventosagendados = (ctx, next) => {
 	}
 	// / dias uteis
 
-	// Qualquer dia
-
-
-	// ------ Mandando relatório mensal para Bartira
-	// var scheduleteste = schedule.scheduleJob({hour: 10+fuso, minute: 31, dayOfWeek: [1,2,3,4,5,6,7], dayOfMonth: 1}, function(){
-	// 	if (pedido.mes_data == 1) {
-	// 		relatorioTempo = [1,12,(pedido.ano_data-1)];
-	// 	} else {
-	// 		relatorioTempo = [1,(pedido.mes_data-1),pedido.ano_data];
-	// 	}
-	// 	exec(ctx, atualizarData, carregartodos, relatoriopao, relatoriopaodetalhado, relatoriopaobartira, liberandopost)
-
-	// });
-
 	var schedulerelatoriomensal = schedule.scheduleJob({hour: 8+fuso, minute: 15}, function(){
+		// Relatório todo dia 1
 		if (datadia == 1) {
 			if (pedido.mes_data == 1) {
 				relatorioTempo = [1,12,(pedido.ano_data-1)];
@@ -1667,6 +1728,13 @@ const eventosagendados = (ctx, next) => {
 				relatorioTempo = [1,(pedido.mes_data-1),pedido.ano_data];
 			}
 			exec(ctx, atualizarData, carregartodos, relatoriopao, relatoriopaodetalhado, relatoriopaobartira, liberandopost)
+		}
+
+		// Niver do bot
+		if (datadia == 17) {
+			if (datames == 9) {
+				msg(`Fui ativado no dia 17 de setembro de 2018. Teoricamente é meu aniversário 🎂`, idChatDegrau);
+			}
 		}
 	  
 	});
@@ -3030,6 +3098,34 @@ setInterval(function() {
 	}
 }, 25 * 60000); // 60 minutos
 
+
+
+
+
+
+
+
+
+
+
+
+bot.command(['cpf'], async ctx => {
+
+	var cpfteste = cpf();
+
+	await ctx.reply(`CPF válido gerado aleatoriamente:
+${cpfteste}`);
+
+})
+
+bot.command(['cnpj'], async ctx => {
+
+	var cnpjteste = cnpj();
+
+	await ctx.reply(`CNPJ válido gerado aleatoriamente:
+${cnpjteste}`);
+
+})
 
 
 
